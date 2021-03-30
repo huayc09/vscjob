993c9f9087f58ce62bc64c77f865512683e8051a
Yichao Hua <61422457+huayc09@users.noreply.github.com>
2021-03-30 15:01:00 +0200

    Initial commit

---
+++ data-raw/Test.R
@@ -0,0 +1,20 @@
+library(vscjob)
+library(Seurat)
+library(dplyr)
+library(mosaic)
+library(rlist)
+library(purrr)
+library(roxygen2)
+library(sinew)
+options(max.print = 50, spe = "mouse", nCores = 12)
+
+usethis::use_data(mouse_human_genesymbols, overwrite = TRUE)
+makeOxygen(RunScenic)
+roxygenize()
+
+
+# value <- list(seu = pbmc, features = "CD3D", group.by = "seurat_clusters", split.by = NULL, cell = NULL,
+#               ncol = 1, scales = "free_y", type = c("violin","boxplot"), outlier.size = 1)
+# for (i in names(value)) {
+#   assign(i, value = value[[i]])
+# }
