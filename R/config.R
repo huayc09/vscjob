b0322c74a0a463194568ee6ce3821224b029d5af
Yichao Hua <61422457+huayc09@users.noreply.github.com>
2021-06-22 16:38:06 +0200

    2021-6-22 update
    Set some config functions

---
+++ R/config.R
@@ -0,0 +1,48 @@
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param force PARAM_DESCRIPTION, Default: FALSE
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @rdname vscjob_CreateConfig
+#' @export
+
+vscjob_CreateConfig <- function(force = FALSE){
+  if(file.exists("~/.vscjob.config") & !force) {
+    stop("Config file already exists.")
+  }
+  file.copy(from = system.file("extdata", ".vscjob.config", package = "vscjob"),
+            to = "~/", overwrite = TRUE)
+}
+
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @rdname vscjob_LoadConfig
+#' @export
+
+vscjob_LoadConfig <- function(){
+  if(!file.exists("~/.vscjob.config")) {
+    vscjob_CreateConfig()
+  }
+  vscjob_options <- read.table("~/.vscjob.config", header = F, sep = "=", row.names = 1)
+  vscjob_options <-
+    setNames(
+      split(vscjob_options$V2, f = seq(nrow(vscjob_options))),
+      nm = rownames(vscjob_options)
+    )
+  assign(x = "vscjob_options", value = vscjob_options, envir = globalenv())
+}
