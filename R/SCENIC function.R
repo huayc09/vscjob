2d80f75748b664bc432c2611b7de9d6cce292b69
Yichao Hua <61422457+huayc09@users.noreply.github.com>
2021-08-09 14:36:24 +0200

    2021-8-9 SCENIC update

--- R/SCENIC function.R
+++ R/SCENIC function.R
@@ -28,22 +28,6 @@ ExportBasicLoom <- function(seu, dir = getwd(), file.name = "seu.loom") {
   lfile <- as.loom(seu_loom,  filename = file.path(dir, file.name))
   lfile$close_all()
 }
-# ExportBasicLoom(seu)
-# ExportBasicLoom(EC_HNSCC, dir = "2021-2-3 Amelie HNSCC EC/", file.name = "EC_HNSCC.loom")
-
-# loom.path = "seu.loom"
-sce.database.path <- list(
-  human = list(
-    TFs = "${VSC_DATA}/SCENIC/databases/hs_hgnc_tfs.txt",
-    motifs = "${VSC_DATA}/SCENIC/databases/motifs-v9-nr.hgnc-m0.001-o0.0.tbl",
-    db = "${VSC_DATA}/SCENIC/databases/hg38__refseq-r80__10kb_up_and_down_tss.mc9nr.feather"
-  ),
-  mouse = list(
-    TFs = "${VSC_DATA}/SCENIC/databases/mm_mgi_tfs.txt",
-    motifs = "${VSC_DATA}/SCENIC/databases/motifs-v9-nr.mgi-m0.001-o0.0.tbl",
-    db = "${VSC_DATA}/SCENIC/databases/mm9-tss-centered-10kb-7species.mc9nr.feather"
-  )
-)
 
 #' @title FUNCTION_TITLE
 #' @description FUNCTION_DESCRIPTION
@@ -91,6 +75,7 @@ RunScenic <- function(
   nodes <- nodes %||% opt$pbs.nodes
   ppn <- ppn %||% opt$pbs.ppn
   nCore <- as.numeric(nodes) * as.numeric(ppn)
+  if(!spe %in% c("mouse","human")) stop("'spe' should be 'mouse' or 'human'")
   ref <- switch(
     spe,
     mouse = list(
@@ -104,6 +89,9 @@ RunScenic <- function(
       db = opt$sce.database.path.human.db
     )
   )
+  check.sys.dir.exist(ref$TFs, nm = "TFs")
+  check.sys.dir.exist(ref$motifs, nm = "motifs")
+  check.sys.dir.exist(ref$db, nm = "SCENIC database")
 
   fileConn <- file(file.path(project.name, "SCENIC_template.sh"))
   writeLines(c(
@@ -139,56 +127,4 @@ RunScenic <- function(
   system(command)
 }
 
-# Old Version
-# RunScenic <- function(
-#   loom.path,
-#   spe = "human",
-#   project.name = "Scenic_project",
-#   dir = getwd(),
-#   nCore = 36,
-#   walltime = "8:00:00") {
-#   input <- gsub(" ", "\\ ", normalizePath(loom.path), fixed = T)
-#   sce_wd <- file.path(dir, project.name)
-#   wd <- gsub(" ", "\\ ", sce_wd, fixed = T)
-#   dir.create(sce_wd)
-#   if(!file.exists(loom.path)) stop("loom.path not found")
-#   fileConn <- file(file.path(project.name, "SCENIC_template.sh"))
-#   writeLines(c(
-#     "#!/usr/bin/bash",
-#     paste0("#PBS -l walltime=", walltime),
-#     "#PBS -l pmem=5gb",
-#     "#PBS -A lp_vsc32982",
-#     paste0("#PBS -l nodes=1:ppn=", nCore),
-#     "#PBS -m ae  # notify on aborted, end",
-#     "#PBS -M yichao.hua@kuleuven.be",
-#
-#     paste0("cd ", wd),
-#     'export PATH="${VSC_DATA}/miniconda3/bin:${PATH}"',
-#     "export NXF_SINGULARITY_CACHEDIR=${VSC_DATA}/singularity_cache/",
-#     "source activate science",
-#
-#     "nextflow run aertslab/SCENICprotocol \\",
-#     "--thr_min_genes 0 \\",
-#     "--thr_min_cells 0 \\",
-#     "--thr_n_genes 999999 \\",
-#     "--thr_pct_mito 1 \\",
-#     paste0("--num_workers ",nCore," \\"),
-#     paste0("--threads ",nCore," \\"),
-#     "-profile singularity \\",
-#     paste0("--loom_input ", input, " \\"),
-#     paste0("--TFs ", sce.database.path[[spe]][["TFs"]], " \\"),
-#     paste0("--motifs ", sce.database.path[[spe]][["motifs"]], " \\"),
-#     paste0("--db ", sce.database.path[[spe]][["db"]])
-#   ), fileConn)
-#   close(fileConn)
-#
-#   command <- paste0("cd ", wd, "\nqsub SCENIC_template.sh")
-#   system(command)
-# }
-
-
-
-# spe = "human"
-# RunScenic(loom.path = "Lung.loom", spe = "human", nCore = 24, project.name = "SCENIC_Lung_Lee")
-# RunScenic(loom.path = "EC_HNSCC.loom", spe = "human", nCore = 24, project.name = "SCENIC_HNSCC")
 # RunScenic(loom.path = "seu.loom", spe = "human", walltime = "0:30:00")
