ea7c776101da7c7ad1abac82197eeadb3b4c50aa
Yichao Hua <61422457+huayc09@users.noreply.github.com>
2021-08-10 12:07:01 +0200

    2021-8-10 update RunRscript

--- R/RunRscript.R
+++ R/RunRscript.R
@@ -1,7 +1,11 @@
 #' @title FUNCTION_TITLE
 #' @description FUNCTION_DESCRIPTION
 #' @param file PARAM_DESCRIPTION
-#' @param nCore PARAM_DESCRIPTION, Default: 36
+#' @param walltime PARAM_DESCRIPTION, Default: NULL
+#' @param project PARAM_DESCRIPTION, Default: NULL
+#' @param nodes PARAM_DESCRIPTION, Default: NULL
+#' @param ppn PARAM_DESCRIPTION, Default: NULL
+#' @param email PARAM_DESCRIPTION, Default: NULL
 #' @return OUTPUT_DESCRIPTION
 #' @details DETAILS
 #' @examples
@@ -13,24 +17,31 @@
 #' @rdname RunRscript
 #' @export
 
-RunRscript <- function(file, nCore = 36) {
-  if(!file.exists(file)) stop("R script file path not found")
+RunRscript <- function(
+  file,
+  walltime = NULL,
+  project = NULL,
+  nodes = NULL,
+  ppn = NULL,
+  email = NULL
+) {
+  check.sys.dir.exist(file, "R script file")
   r.file <- gsub(" ", "\\ ", normalizePath(file), fixed = T)
   wd <- gsub(" ", "\\ ", getwd(), fixed = T)
   sh.name <- "rscript.sh"
   fileConn <- file(sh.name)
   writeLines(c(
-    "#!/usr/bin/bash",
-    "#PBS -l walltime=1:00:00",
-    "#PBS -l pmem=5gb",
-    "#PBS -A lp_vsc32982",
-    paste0("#PBS -l nodes=1:ppn=", nCore),
-    "#PBS -m ae  # notify on aborted, end",
-    "#PBS -M yichao.hua@kuleuven.vib.be",
+    pbs.string(
+      walltime = walltime,
+      project = project,
+      nodes = nodes,
+      ppn = ppn,
+      email = email
+    ),
 
     paste0("cd ", wd),
     'export PATH="${VSC_DATA}/miniconda3/bin:${PATH}"',
-    "source activate general",
+    "conda activate general",
 
     paste0("Rscript ", r.file)
   ), fileConn)
@@ -38,3 +49,29 @@ RunRscript <- function(file, nCore = 36) {
 
   system("qsub rscript.sh")
 }
+
+# RunRscript <- function(file, nCore = 36) {
+#   if(!file.exists(file)) stop("R script file path not found")
+#   r.file <- gsub(" ", "\\ ", normalizePath(file), fixed = T)
+#   wd <- gsub(" ", "\\ ", getwd(), fixed = T)
+#   sh.name <- "rscript.sh"
+#   fileConn <- file(sh.name)
+#   writeLines(c(
+#     "#!/usr/bin/bash",
+#     "#PBS -l walltime=1:00:00",
+#     "#PBS -l pmem=5gb",
+#     "#PBS -A lp_vsc32982",
+#     paste0("#PBS -l nodes=1:ppn=", nCore),
+#     "#PBS -m ae  # notify on aborted, end",
+#     "#PBS -M yichao.hua@kuleuven.vib.be",
+#
+#     paste0("cd ", wd),
+#     'export PATH="${VSC_DATA}/miniconda3/bin:${PATH}"',
+#     "source activate general",
+#
+#     paste0("Rscript ", r.file)
+#   ), fileConn)
+#   close(fileConn)
+#
+#   system("qsub rscript.sh")
+# }
